27719c849043cad96d7bd11978091966d8611237
brprod8 <101079914+brprod8@users.noreply.github.com>
2022-03-27 16:20:27 -0700

    Add files via upload

--- conditional_statement.py
+++ conditional_statement.py
@@ -5,11 +5,27 @@
 # and is already registered on the platform.
 # YOUR CODE HERE!
 
+location = "United States"
+is_registered = True
+
 # 2. @TODO: Write a conditional statement. If user is based in the U.S. and
 # already registered, advertise the new service. Otherwise, give them
 # notice that the feature is coming soon.
 # YOUR CODE HERE!
+if location == "United States" and is_registered:
+    print("Now Available! Crypto Accepted!")
+
+else:
+    print("Crypto Payments - Coming Soon!")
 
 # 3. @TODO: Change the user location to Japan and print the results again.
 # Does your conditional statement program work as expected?
 # YOUR CODE HERE!
+location = "Japan"
+is_registered = True
+
+if location == "United States" and is_registered is True:
+    print("Now Available! Crypto Accepted!")
+
+else:
+    print("Crypto Payments - Coming Soon!")
